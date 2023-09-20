a77e94a02646cb8e087d91b28c919a7a559a8651
Ricardo Presilla <presilla.r@opendevpro.com>
2023-09-20 12:13:10 -0300

    Begin repository

---
+++ src/main/java/com/alfaCentauri/Drools/ExampleDroolsApplication.java
@@ -0,0 +1,13 @@
+package com.alfaCentauri.Drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleDroolsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alfaCentauri/Drools/exceptions/DroolException.java
@@ -0,0 +1,15 @@
+package com.alfaCentauri.Drools.exceptions;
+
+public class DroolException extends Exception{
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public DroolException(String message) {
+        super(message);
+    }
+}
